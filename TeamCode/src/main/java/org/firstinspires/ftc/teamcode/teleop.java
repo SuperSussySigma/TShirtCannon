f6552bad949d5e0b3fba41169ea94d048c8cb0f6
Alejandro <147785225+ben-coughlin@users.noreply.github.com>
2026-01-26 16:22:21 -0700

    lowkirkuinely testing the git repo from new computer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -170,7 +170,7 @@ public class teleop extends OpMode
         leftFront.setPower(leftPower);
         rightfront.setPower(rightPower);
         rightback.setPower(rightPower);
-
+        //gkjgkj
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
